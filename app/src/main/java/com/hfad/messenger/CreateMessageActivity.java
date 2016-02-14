93d341fd3bcff4a60e530df7fd1929ef2c5a6ed6
AM1A <boldyhappy123@gmail.com>
2016-02-14 10:29:41 +0530

    My messenger app commit

---
+++ app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -0,0 +1,26 @@
+package com.hfad.messenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+    public void onSendMessage(View view){
+        EditText messageView=(EditText)findViewById(R.id.message);
+        String messageText=messageView.getText().toString();
+        Intent intent=new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,messageText);
+        String chooserTitle=getString(R.string.chooser);
+        Intent chosenIntent=Intent.createChooser(intent,chooserTitle);
+        startActivity(chosenIntent);
+
+    }
+}
